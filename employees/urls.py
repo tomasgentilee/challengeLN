4e991de325006176d29a76879d3a39a09df9ee1e
Tomas Gentile <tomigentile@gmail.com>
2025-10-01 12:56:59 -0300

    router

--- employees/urls.py
+++ employees/urls.py
@@ -2,6 +2,6 @@ from rest_framework.routers import DefaultRouter
 from .views import EmployeeViewSet
 
 router = DefaultRouter()
-router.register(r"empleados", EmployeeViewSet, basename="empleados")
+router.register(r"employees", EmployeeViewSet, basename="employees")
 
 urlpatterns = router.urls
